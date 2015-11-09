ded8098bdc04dfb27b01f7c72f713bb517c71be6
Roberto Antolin <roberto.antolin@roslin.ed.ac.uk>
2015-11-09 16:44:32 +0000

    Impute alleles after calculate most likely path for gameteHMM

--- hmmHaplotype.f90
+++ hmmHaplotype.f90
@@ -1,106 +1,161 @@
-subroutine ForwardAlgorithmForHaplotype(CurrentInd)
+subroutine ForwardAlgorithmForHaplotype(CurrentInd, hap)
 use Global
 use GlobalVariablesHmmMaCH
 use Par_Zig_mod
 use omp_lib
 
 implicit none
-integer, intent(in) :: CurrentInd
+integer, intent(in) :: CurrentInd, hap
+! double precision, intent(IN), allocatable :: Thetas(:)
 
 ! Local variables
-integer :: i
+integer :: marker
 
-i = 1
+marker = 1
 call SetUpPriorHaplotype
-call ConditionHaplotypeOnData(CurrentInd, i, SubH(i,Marker))
+call ConditionHaplotypeOnData(CurrentInd, marker, PhaseHmmMaCH(CurrentInd,marker,hap))
 
-do i=2,nSnpHmm
-	call GetSmallMemoryBlock
-	call ConditionHaplotypeOnData(CurrentInd, i, SubH(i,Marker))
+do marker=2,nSnpHmm
+    ! call GetSmallMemoryBlock
+    call TransposeHaplotype(marker-1, marker, Thetas(marker-1))
+    call ConditionHaplotypeOnData(CurrentInd, marker, PhaseHmmMaCH(CurrentInd,marker,hap))
 enddo
 
 end subroutine ForwardAlgorithmForHaplotype
 
 !######################################################################
-subroutine SampleHaplotypeSource(CurrentInd)
+subroutine SampleHaplotypeSource(CurrentInd,hap)
 use Global
 use GlobalVariablesHmmMaCH
 use Par_Zig_mod
 use omp_lib
 
 implicit none
-integer,intent(IN) :: CurrentInd
+integer,intent(IN) :: CurrentInd, hap
 
 ! Local variables
-integer :: state, marker, Thread
+integer :: i, state, marker, Thread
 
 ! double precision :: Probs(nHapInSubH*(nHapInSubH+1)/2)
 double precision :: Probs(nHapInSubH)
 double precision :: Summer, Choice, Theta, cross, nocross
 
-
 Thread = omp_get_thread_num()
 Summer=0.0
 Probs = ForwardProbs(:,nSnpHmm)
 
+! Calculate sum over all states
 do state=1,nHapInSubH
-	Summer = Summer + Probs(state)
+    Summer = Summer + Probs(state)
 enddo
 
-Summer=0.0
+! Sample number and select state
 Choice = par_uni(Thread)*Summer
+Summer=0.0
 
-do haplotype=1,nHapInSubH
-	Summer = Summer + Probs(haplotype)
-	if (Summer >= Choice) exit
+do i=1,nHapInSubH
+    Summer = Summer + Probs(i)
+    if (Summer >= Choice) exit
 enddo
 
 do marker=nSnpHmm-1,1,-1
-	if (SubH(haplotype,marker)==SubH(nHapInSubH,marker)) then
-		ErrorMatches(marker)=ErrorMatches(marker)+1
-	else
-		ErrorMismatches(marker)=ErrorMismatches(marker)+1
-	endif
+    ! Track whether imputed state matches observed allele
+    if (SubH(i,marker)==PhaseHmmMaCH(CurrentInd,marker,hap)) then
+        ErrorMatches(marker)=ErrorMatches(marker)+1
+    else
+        ErrorMismatches(marker)=ErrorMismatches(marker)+1
+    endif
 
-	Theta = Thetas(marker)
-	Probs = ForwardProbs(:,marker)
+    ! Impute if allele is missing
+    if (PhaseHmmMaCH(CurrentInd,marker,hap)==3) then
+        FullH(CurrentInd,marker,hap) = SubH(i,marker)
+        ! call ImputeAllele(CurrentInd,marker,i,hap)
+    endif
 
-	nocross = Probs(haplotype) * (1.0 - Theta)
-	Summer = 0.0
+    Theta = Thetas(marker)
+    Probs = ForwardProbs(:,marker)
 
-	do i=1,nHapInSubH
-		Summer = Summer + Probs(i)
-	enddo
+    nocross = Probs(i) * (1.0 - Theta)
+    Summer = 0.0
 
-	cross = Summer * Theta / nHapInSubH
+    do i=1,nHapInSubH
+        Summer = Summer + Probs(i)
+    enddo
 
-	! Sample number and decide how many state changes occurred between the
-	! two positions
-	Choice = par_uni(Thread)*(nocross+cross)
+    cross = Summer * Theta / nHapInSubH
 
-	! The most likely outcome is that no changes occur ...
-	if (Choice <= nocross) continue
+    ! Sample number and decide how many state changes occurred between the
+    ! two positions
+    Choice = par_uni(Thread)*(nocross+cross)
 
-	! TODO: Look what crossovers are
-	crossovers(i)= crossovers(i)+1
+    ! The most likely outcome is that no changes occur ...
+    if (Choice <= nocross) continue
 
-	! If a crossover occured, we need to sample a state according to probability
-	Choice = par_uni(Thread)*(Summer)
+    ! TODO: Look what crossovers are
+    crossovers(i)= crossovers(i)+1
 
-	Summer = 0.0
-	do haplotype=1,nHapInSubH
-		Summer = Summer + Probs(haplotype)
-		if (Summer >= Choice) exit
-	enddo
+    ! If a crossover occured, we need to sample a state according to probability
+    Choice = par_uni(Thread)*(Summer)
+
+    Summer = 0.0
+    do i=1,nHapInSubH
+        Summer = Summer + Probs(i)
+        if (Summer >= Choice) exit
+    enddo
 enddo
 
-if (SubH(haplotype,1)==SubH(haplotype,1)) then
-	ErrorMatches(1)=ErrorMatches(marker)+1
+! Track whether imputed state matches observed allele
+if (SubH(i,1)==PhaseHmmMaCH(CurrentInd,1,hap)) then
+    ErrorMatches(1)=ErrorMatches(1)+1
 else
-	ErrorMismatches(1)=ErrorMismatches(marker)+1
+    ErrorMismatches(1)=ErrorMismatches(1)+1
+endif
+
+! Impute if allele is missing
+if (PhaseHmmMaCH(CurrentInd,1,hap)==3) then
+    FullH(CurrentInd,1,hap) = SubH(i,1)
+    ! ImputeAllele(CurrentInd,1,i,hap)
 endif
 
 end subroutine SampleHaplotypeSource
+
+!######################################################################
+subroutine TransposeHaplotype(PrecedingMarker, CurrentMarker, Theta)
+! Calculates the probability of get a particular state at CurrentMarker
+! from any other state at PrecedingMarker using the transition probabilities.
+use GlobalVariablesHmmMaCH
+
+integer, intent(IN) :: PrecedingMarker, CurrentMarker
+double precision, intent(IN) :: Theta
+
+! Local variables
+integer :: i
+double precision :: Summer, NoChange, OneChange
+
+if (Theta==0.0) then
+    do i=1,nHapInSubH
+        ForwardProbs(i,CurrentMarker)=ForwardProbs(i,PrecedingMarker)
+    enddo
+    return
+endif
+
+! Summer=0.0
+Summer=sum(ForwardProbs(:,PrecedingMarker))
+NoChange=1.0-Theta
+OneChange=Summer*Theta/nHapInSubH
+
+! Automatically rescale likelihoods when they get too small
+if (Summer < 1e-15) then
+    NoChange=NoChange*1e30
+    OneChange=OneChange*1e30
+endif
+
+! This final loop actually transposes the probabilities for each state
+do i=1,nHapInSubH
+    ForwardProbs(i,CurrentMarker)=ForwardProbs(i,PrecedingMarker)*NoChange+OneChange
+enddo
+
+end subroutine TransposeHaplotype
 
 !######################################################################
 subroutine ConditionHaplotypeOnData(Marker, allele)
