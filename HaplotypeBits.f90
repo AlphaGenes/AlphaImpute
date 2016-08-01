aa7da42c1c856be1a7d41b2ffbe230066d460657
ANTOLIN Roberto <rantolin@mvm-ri-m145251.roslin.ed.ac.uk>
2016-08-01 17:12:14 +0100

    New definition to compare haps and headers

--- HaplotypeBits.f90
+++ HaplotypeBits.f90
@@ -29,12 +29,13 @@
 !-----------------------------------------------------------------------------------------------------------------------
 MODULE HaplotypeBits
   implicit none
-  PUBLIC
+  PRIVATE
 
-!  PUBLIC compareHaplotype, compareHaplotypeAllowMissing
+  PUBLIC compareHaplotype, compareHaplotypeAllowMissing
+  PUBLIC BitCountAlleleImputed, BitCountAllelePhased
   
   INTERFACE compareHaplotype
-    MODULE PROCEDURE compareHaplotypeThreshold, compareHaplotypeExtrict
+    MODULE PROCEDURE compareHaplotypeThreshold, compareHaplotypeExtrict, compareHaplotype2Threshold, compareHaplotype2Extrict
   END INTERFACE compareHaplotype
 !
   INTERFACE compareHaplotypeAllowMissing
@@ -167,6 +168,7 @@ CONTAINS
 !> @param[in]  miss1  Bit-wise array with the missing alleles of haplotype 1
 !> @param[in]  miss2  Bit-wise array with the missing alleles of haplotype 2
 !> @param[in]  nSecs  Number of elements of the bit-wise arrays
+!> @param[in]  thres  Number of alleles allow to differ
 !> @return     .TRUE. if haplotypes are the same, .FALSE. otherwise 
 !---------------------------------------------------------------------------  
   FUNCTION compareHaplotypeAllowMissingThreshold(hap1, hap2, miss1, miss2, nSecs, thres) result(same)
@@ -193,28 +195,94 @@ CONTAINS
 
   END FUNCTION compareHaplotypeAllowMissingThreshold
 
-!!#############################################################################################################################################################################################################################
-!  FUNCTION compareHaplotype2(hap1, hap2, miss, nSecs) result(c)
-!    integer(kind=8), dimension(:), intent(in) :: hap1, hap2, miss
-!    integer, intent(in) :: nSecs
-!    integer :: c
+!---------------------------------------------------------------------------
+! DESCRIPTION:
+!> @brief      Compare two haplotypes
 !
-!    integer :: i
+!> @details    Return .FALSE. if at least a number of alleles (thres) of hap1 is
+!>             different from the homologous allele of hap2, and .TRUE. otherwise.
+!>
+!>             Note: This function skips missing alleles from alleles given by miss
 !
-!    c = 0
-!    do i = 1, nSecs
-!      c = c + POPCNT( IAND( IEOR(hap1(i), hap2(i)),&
-!                            NOT(miss(i))))
-!      if (c/=0) then
-!        c=i
-!        exit
-!      end if
-!    end do
+!> @author     Roberto Antolin, roberto.antolin@roslin.ed.ac.uk
 !
-!  END FUNCTION compareHaplotype2
+!> @date        Aug 01, 2016
+!
+! PARAMETERS:
+!> @param[in]  hap1   Bit-wise array with the first haplotype to compare
+!> @param[in]  hap2   Bit-wise array with the second haplotype to compare
+!> @param[in]  miss   Bit-wise array with the missing alleles of haplotype 1
+!> @param[in]  nSecs  Number of elements of the bit-wise arrays
+!> @param[in]  thres  Number of alleles allow to differ
+!> @return     .TRUE. if haplotypes are the same, .FALSE. otherwise
+!---------------------------------------------------------------------------
+  FUNCTION compareHaplotype2Threshold(hap1, hap2, miss, nSecs, thres) result(same)
+    integer(kind=8), dimension(:), intent(in) :: hap1, hap2, miss
+    integer, intent(in) :: nSecs, thres
+    logical :: same
 
-!#############################################################################################################################################################################################################################
+    integer :: i, c
 
+    c = 0
+    same = .TRUE.
+    do i = 1, nSecs
+      c = c + POPCNT( IAND( IEOR(hap1(i), hap2(i)),&
+                            NOT(miss(i))))
+      if ( c >= thres ) then
+        same = .FALSE.
+        exit
+      end if
+    end do
+
+  END FUNCTION compareHaplotype2Threshold
+
+!---------------------------------------------------------------------------
+! DESCRIPTION:
+!> @brief      Compare two haplotypes
+!
+!> @details    Return .FALSE. if at least an allele (thres) of hap1 is
+!>             different from the homologous allele of hap2, and .TRUE. otherwise.
+!>
+!>             Note: This function skips missing alleles from alleles given by miss
+!
+!> @author     Roberto Antolin, roberto.antolin@roslin.ed.ac.uk
+!
+!> @date        Aug 01, 2016
+!
+! PARAMETERS:
+!> @param[in]  hap1   Bit-wise array with the first haplotype to compare
+!> @param[in]  hap2   Bit-wise array with the second haplotype to compare
+!> @param[in]  miss   Bit-wise array with the missing alleles of haplotype 1
+!> @param[in]  nSecs  Number of elements of the bit-wise arrays
+!> @return     .TRUE. if haplotypes are extrictly the same, .FALSE. otherwise
+!---------------------------------------------------------------------------
+  FUNCTION compareHaplotype2Extrict(hap1, hap2, miss, nSecs) result(same)
+    integer(kind=8), dimension(:), intent(in) :: hap1, hap2, miss
+    integer, intent(in) :: nSecs
+    logical :: same
+
+    same = .TRUE.
+    same = compareHaplotype2Threshold(hap1, hap2, miss, nSecs, 1)
+
+  END FUNCTION compareHaplotype2Extrict
+
+!---------------------------------------------------------------------------
+! DESCRIPTION:
+!> @brief      Count the total number of markers completely phased
+!
+!> @details    Count the total number of alleles completely phased
+!>             for a pair of haplotyes
+!
+!> @author     Roberto Antolin, roberto.antolin@roslin.ed.ac.uk
+!
+!> @date        Aug 01, 2016
+!
+! PARAMETERS:
+!> @param[in]  miss1  Bit-wise array with the missing alleles of haplotype 1
+!> @param[in]  miss2  Bit-wise array with the missing alleles of haplotype 2
+!> @param[in]  nSecs  Number of elements of the bit-wise arrays
+!> @return     Number of markers phased
+!---------------------------------------------------------------------------
   FUNCTION BitCountAllelePhased(miss1, miss2, nSecs) result(c)
     integer(kind=8), dimension(:), intent(in) :: miss1, miss2
     integer, intent(in) :: nSecs
@@ -229,8 +297,21 @@ CONTAINS
 
   END FUNCTION BitCountAllelePhased
 
-!#############################################################################################################################################################################################################################
-
+!---------------------------------------------------------------------------
+! DESCRIPTION:
+!> @brief      Count the total number of alleles completely imputed
+!
+!> @details    Count the total number of alleles completely imputed in a haplotype
+!
+!> @author     Roberto Antolin, roberto.antolin@roslin.ed.ac.uk
+!
+!> @date        Aug 01, 2016
+!
+! PARAMETERS:
+!> @param[in]  miss  Bit-wise array with the missing alleles of haplotype
+!> @param[in]  nSecs  Number of elements of the bit-wise arrays
+!> @return     Number of alleles imputed
+!---------------------------------------------------------------------------
   FUNCTION BitCountAlleleImputed(miss, nSecs) result(c)
     integer(kind=8), dimension(:), intent(in) :: miss
     integer, intent(in) :: nSecs
@@ -244,8 +325,5 @@ CONTAINS
     end do
 
   END FUNCTION BitCountAlleleImputed
-
-
-
 
 END MODULE HaplotypeBits
