3892440a77d9e04cf9d40a6e1f744c462349a67f
WILSON David <david.wilson@roslin.ed.ac.uk>
2016-12-21 15:08:14 +0000

    removed allocation statements

--- src/Imputation.f90
+++ src/Imputation.f90
@@ -665,9 +665,9 @@ end subroutine InternalParentPhaseElim
           !$OMP DEFAULT(SHARED) &
           !$OMP PRIVATE(i,j,e,h,HapElim,BanBoth,Counter,Count0,Count1,Ban,curPos,curSection,BitWork,MissWork,BitGeno)
           do i=1,nAnisP
-            allocate(BitWork(numSections,2))
-            allocate(MissWork(numSections,2))
-            allocate(HapElim(nAnisP*2,2))
+            ! allocate(BitWork(numSections,2))
+            ! allocate(MissWork(numSections,2))
+            ! allocate(HapElim(nAnisP*2,2))
             HapElim=1
             BanBoth=0
             BitWork = 0
@@ -796,9 +796,9 @@ end subroutine InternalParentPhaseElim
                 enddo
               endif
             enddo
-            deallocate(HapElim)
-            deallocate(BitWork)
-            deallocate(MissWork)
+            ! deallocate(HapElim)
+            ! deallocate(BitWork)
+            ! deallocate(MissWork)
           enddo
           !$OMP END PARALLEL DO
 
