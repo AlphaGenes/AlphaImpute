755047a1280851db0ca5804f33c2e0d4415ffe6f
David Wilson <david.wilson@roslin.ed.ac.uk>
2018-03-07 09:52:58 +0000

    added debug statements

--- src/Imputation.f90
+++ src/Imputation.f90
@@ -1799,6 +1799,8 @@ MODULE Imputation
 			type(individual), pointer :: tmpChild
 			integer(kind=1) :: phase1,phase2, childphase
 			inputParams => defaultInput
+
+			print *,
 			! TODO maybe change this to sireList?
 			do i=1,ped%pedigreeSize - ped%nDummys
 				if (ped%pedigree(i)%nOffs /= 0) then       ! check that animal i,j is a sire or a dam
@@ -1903,6 +1905,7 @@ MODULE Imputation
 								endif
 
 
+								print *,"OFFSPRING AT POINT:",ped%pedigree(i)%nOffs
 								do l=1,ped%pedigree(i)%nOffs
 									tmpChild => ped%pedigree(i)%offsprings(l)%p
 
